b250b510905d8806ad2c05f93f9d41ae8f071ee8
Michael Joseph <alotoftype@gmail.com>
2017-01-31 22:32:18 -0500

    added codekit to the project

---
+++ page-templates/homepage.php
@@ -0,0 +1,24 @@
+<?
+
+/*Template Name: HomePage Template */
+
+?>
+
+
+<? get_header(); ?>
+
+<section id="about">
+    <p>about</p>
+</section>
+<section id="services">
+    <p>services</p>
+</section>
+<section id="contact">
+    <p>contact</p>
+</section>
+<section id="news">
+    <p>news</p>
+</section>
+
+
+<? get_footer(); ?>
